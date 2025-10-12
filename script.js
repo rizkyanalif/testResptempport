5863332c02cbf5900c575db0fff923d05cdc26ad
rizkyanalif <malikulsyah@gmail.com>
2025-10-12 12:05:06 +0700

    navbar finish

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const navbarNav = document.querySelector('.navbar-nav')
+
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active')
 }
+
+const hamburgerMenu = document.querySelector('#hamburger-menu')
+document.addEventListener('click', function(e){
+    if(!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active')
+    }
+}) 
