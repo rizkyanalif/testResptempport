7e6d404f1ce810dafa0e35e7a8d0d6f0e2d81b82
rizkyanalif <malikulsyah@gmail.com>
2025-10-12 17:18:02 +0700

    highlight when scroll

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ document.addEventListener('click', function(e){
         navbarNav.classList.remove('active')
     }
 }) 
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.navbar .navbar-nav a');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100; 
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove('highlight');
+    if (link.getAttribute('href') === `#${current}`) {
+      link.classList.add('highlight');
+    }
+  });
+});
