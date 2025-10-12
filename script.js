f8134922af364319a6b8be9fb08a7b8247483eca
rizkyanalif <malikulsyah@gmail.com>
2025-10-12 17:19:20 +0700

    adding the js for prev feature

--- script.js
+++ script.js
@@ -9,4 +9,27 @@ document.addEventListener('click', function(e){
     if(!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active')
     }
-}) 
+})
+
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.navbar .navbar-nav a');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100; 
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove('highlight');
+    if (link.getAttribute('href') === `#${current}`) {
+      link.classList.add('highlight');
+    }
+  });
+});
